89063eafa291fb38b336c4dbbb7461e5ff3323e6
NeoMaster831 <72684393+NeoMaster831@users.noreply.github.com>
2020-11-21 15:48:56 +0900

    효율적인 알고리즘
    lol it works in (t = 2 billion)

--- e_3/is_genius.py
+++ e_3/is_genius.py
@@ -1,66 +1,18 @@
-size = list(map(int, input().split())) # size = [ x, y ]
-tmpx, tmpy = list(map(int, input().split()))
-fort = int(input())
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
 
-mode = 1
+psw = 1
+qsw = 1
 
-# mode 1 = (+, +)  ↗
-# mode 2 = (-, +)  ↖
-# mode 3 = (+, -)  ↘
-# mode 4 = (-, -)  ↙
+for i in range(t % (w*2)):
+    p += psw
+    if p >= w or p <= 0:
+        psw *= -1 
 
-for i in range(fort):
-    tmpx_ = tmpx; tmpy_ = tmpy
-    if mode == 1:
-        tmpx_ += 1;  tmpy_ += 1
+for i in range(t % (h*2)):
+    q += qsw
+    if q >= h or q <= 0:
+        qsw *= -1
 
-        # case 1: tmpx > size[0]
-        if tmpx_ > size[0]:
-            mode = 2
-            tmpx -= 1; tmpy += 1
-
-        # case 2: tmpy > size[0]
-        if tmpy > size[1]:
-            mode = 3
-            tmpx += 1; tmpy -= 1
-    
-    elif mode == 2:
-        tmpx_ -= 1; tmpy_ += 1
-
-        # case 1: tmpx < 0
-        if tmpx_ < 0:
-            mode = 1
-            tmpx += 1; tmpy += 1
-        
-        # case 2: tmpy > size[1]
-        if tmpy_ > size[1]:
-            mode = 4
-            tmpx -= 1; tmpy -= 1
-    
-    elif mode == 3:
-        tmpx_ += 1; tmpy_ -= 1
-
-        # case 1: tmpx > size[0]
-        if tmpx_ > size[0]:
-            mode = 4
-            tmpx -= 1; tmpy -= 1
-        
-        # case 2: tmpy < 0
-        if tmpy_ < 0:
-            mode = 1
-            tmpx += 1; tmpy += 1
-    
-    else:
-        tmpx_ -= 1; tmpy_ -= 1
-
-        # case 1: tmpx < 0
-        if tmpx_ < 0:
-            mode = 3
-            tmpx += 1; tmpy -= 1
-        
-        #case 2: tmpy < 0
-        if tmpy_ < 0:
-            mode = 2
-            tmpx -= 1; tmpy += 1
-
-print(tmpx, tmpy)
+print(p,q)
